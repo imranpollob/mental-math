c70c1f2a59e2e9a0655cc7fd94a45bfa319a0a7b
Imran <polboy777@gmail.com>
2019-12-13 20:14:46 +0600

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,58 @@
 <template>
   <div class="home">
     <form @submit.prevent="check">
-      <span class="first-arg" >{{first}}</span>
+      <span class="first-arg">{{first}}</span>
       <span class="operator">{{operator}}</span>
-      <span class="first-arg">{{second}}</span>
-      <input type="number" v-model="answer">
-      <div>{{feedback}}</div>
+      <span class="second-arg">{{second}}</span>
+      =
+      <input type="number" class="answer" v-model="answer" @keydown="typing"/>
+      <div class="feedback">{{feedback}}</div>
     </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  },
+  name: "home",
   data() {
     return {
       first: 1,
       second: 2,
-      operator: '+',
+      operator: "+",
       answer: null,
-      feedback: ''
-    }
+      feedback: ""
+    };
   },
   methods: {
     check() {
-      this.feedback = this.first + this.second == this.answer ? 'Correct' : 'Opps'
-      if (this.feedback == 'Correct') {
-        this.first += 1 
-        this.second += 1 
+      this.feedback =
+        this.first + this.second == this.answer ? "Correct" : "Opps";
+
+      if (this.feedback == "Correct") {
+        // setTimeout(() => {
+          this.first += 1;
+          this.second += 1;
+          this.answer = null;
+        // }, 1000);
       }
+    },
+    typing() {
+      this.feedback = ''
     }
-  },
-}
+  }
+};
 </script>
+
+<style scoped>
+.first-arg,
+.second-arg,
+.oparator,
+.answer,
+.feedback {
+  font-size: 24px;
+}
+
+.feedback{
+  transition: opacity 1s linear;;
+}
+</style>
