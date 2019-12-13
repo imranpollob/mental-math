1a206f082c9fe68648f2dd8edeea68f6a8985610
Imran <polboy777@gmail.com>
2019-12-13 19:53:51 +0600

    initail commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="check">
+      <span class="first-arg" >{{first}}</span>
+      <span class="operator">{{operator}}</span>
+      <span class="first-arg">{{second}}</span>
+      <input type="number" v-model="answer">
+      <div>{{feedback}}</div>
+    </form>
   </div>
 </template>
 
@@ -13,6 +18,20 @@ export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      first: 1,
+      second: 2,
+      operator: '+',
+      answer: null,
+      feedback: ''
+    }
+  },
+  methods: {
+    check() {
+      this.feedback = this.first + this.second == this.answer ? 'Correct' : 'Opps'
+    }
+  },
 }
 </script>
